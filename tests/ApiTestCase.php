15601ed0bba57297cfbb549556bea52fb4f36ce3
Gary Clarke <garyclarke1974@gmail.com>
2023-06-25 20:44:04 +0100

    Work out application sequence

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -14,6 +14,18 @@ abstract class ApiTestCase extends BaseTestCase
         array $headers = []
     ): Response
     {
+        // Json encode the data
+
+        // Merge server variables with $headers
+
+        // Create a $request using a static named constructor
+
+        // Create / resolve the Kernel
+
+        // Obtain a $response object: $response = $kernel->handle($request)
+
+        // return the $response
+
         $body = '{"id":1,"title":"Clean Code: A Handbook of Agile Software Craftsmanship","year_published":2008,"author":{"id":1,"name":"Robert C. Martin","bio":"This is an author"}}';
 
         return new Response(body: $body, statusCode: 200, headers: []);
