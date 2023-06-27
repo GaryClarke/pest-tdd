80df32df9c0e4d56679bb33a663514bd13daeea3
Gary Clarke <garyclarke1974@gmail.com>
2023-06-27 16:47:48 +0100

    Add query params to Request construction

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -4,6 +4,12 @@ namespace App\Http;
 
 class Request
 {
+    public function __construct(
+        private array $queryParams, // $_GET
+    )
+    {
+    }
+
     public static function create(
         string $method,
         string $uri,
@@ -11,6 +17,16 @@ class Request
         string $content
     ): self
     {
-        return new self();
+        $uriParts = parse_url($uri);
+        parse_str($uriParts['query'] ?? '', $queryParams);
+
+        return new self(
+            $queryParams
+        );
+    }
+
+    public function getQueryParams(): array
+    {
+        return $this->queryParams;
     }
 }
