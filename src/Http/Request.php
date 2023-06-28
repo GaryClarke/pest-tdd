fb807a1c84cb54ca65c14675dbe1c558f6d7b85e
Gary Clarke <garyclarke1974@gmail.com>
2023-06-28 15:57:08 +0100

    Add getPath()

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -6,6 +6,7 @@ class Request
 {
     public function __construct(
         private array $queryParams, // $_GET
+        private array $serverVars = []
     )
     {
     }
@@ -13,20 +14,29 @@ class Request
     public static function create(
         string $method,
         string $uri,
-        iterable $server,
+        array $server,
         string $content
     ): self
     {
         $uriParts = parse_url($uri);
         parse_str($uriParts['query'] ?? '', $queryParams);
 
+        $_SERVER['REQUEST_URI'] = $uri;
+        $serverVars = array_merge($server, $_SERVER);
+
         return new self(
-            $queryParams
+            $queryParams,
+            $serverVars
         );
     }
 
     public function getQueryParams(): array
     {
         return $this->queryParams;
+    }
+
+    public function getPath(): string
+    {
+        return strtok($this->serverVars['REQUEST_URI'], '?');
     }
 }
