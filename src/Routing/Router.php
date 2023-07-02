2f242fc2862dd697c4615cbf13faa444478ff013
Gary Clarke <garyclarke1974@gmail.com>
2023-07-02 20:09:02 +0100

    Add the router dispatch method

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -2,7 +2,13 @@
 
 namespace App\Routing;
 
+use App\Http\Request;
+use App\Http\Response;
+
 class Router
 {
-
+    public function dispatch(Request $request): Response
+    {
+        return new Response();
+    }
 }
