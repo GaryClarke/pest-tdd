62afa2fd105be55bd5c441b89a4d18dd31759553
Gary Clarke <garyclarke1974@gmail.com>
2023-07-03 20:16:00 +0100

    Add FastRoute and incorporate in Router::dispatch

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -4,11 +4,43 @@ namespace App\Routing;
 
 use App\Http\Request;
 use App\Http\Response;
+use FastRoute\Dispatcher;
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
 
 class Router
 {
     public function dispatch(Request $request): Response
     {
-        return new Response();
+        $dispatcher = simpleDispatcher(function (RouteCollector $r) {
+            $r->addRoute('GET', '/foo', function () {
+                return new Response();
+            });
+        });
+
+        // Fetch method and URI from somewhere
+        $httpMethod = $request->getMethod();
+        $uri = $request->getPath();
+
+        $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+        switch ($routeInfo[0]) {
+            case Dispatcher::NOT_FOUND:
+                // ... 404 Not Found
+                break;
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = $routeInfo[1];
+                // ... 405 Method Not Allowed
+                break;
+            case Dispatcher::FOUND:
+                $handler = $routeInfo[1];
+
+                $response = $handler();
+                //$vars = $routeInfo[2];
+                // ... call $handler with $vars
+                break;
+        }
+
+        return $response;
     }
 }
