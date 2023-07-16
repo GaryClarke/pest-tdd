48b1d8ace46e5567f2c8f55cb5bf2c9d4e34218a
Gary Clarke <garyclarke1974@gmail.com>
2023-07-16 19:34:26 +0100

    Add steps to BookRepository

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -6,6 +6,12 @@ class BookRepository
 {
     public function findById(int $id): ?Book
     {
+        // Retrieve book data from the database
 
+        // Use it to create and hydrate a Book
+        $book = Book::create();
+
+        // return the Book
+        return $book;
     }
 }
