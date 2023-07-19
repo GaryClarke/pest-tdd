22231f23db51f1afb50842cf64c0640fe7d5b057
Gary Clarke <garyclarke1974@gmail.com>
2023-07-19 20:27:54 +0100

    Remove json

--- tests/Unit/BookTest.php
+++ tests/Unit/BookTest.php
@@ -29,10 +29,3 @@ test('a book can be created using a named constructor', function() {
 
     ;
 });
-
-
-
-
-
-
-$body = '{"id":1,"title":"Clean Code: A Handbook of Agile Software Craftsmanship","year_published":2008,"available_since":"May 2023","author":{"id":1,"name":"Robert C. Martin","bio":"This is an author"}}';
