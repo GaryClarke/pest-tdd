8751a7ee33072c73af305ef961754fe760ba3c5e
Gary Clarke <garyclarke1974@gmail.com>
2023-07-22 17:44:24 +0100

    Add PDO

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -4,20 +4,47 @@ namespace App\Repository;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use PDO;
 
 class BookRepository
 {
     public function findById(int $id): ?Book
     {
-        // Retrieve book data from the database
-        $row = [
-            'id' => 999,
-            'title' => 'A Test Book',
-            'year_published' => 1999,
-            'author_id' => 123,
-            'author_name' => 'A. N. Author',
-            'author_bio' => 'This is an author'
-        ];
+        // Instantiate a PDO instance
+        $dsn = '';
+        $pdo = new PDO($dsn);
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+
+        // Prepare the statement (SQL)
+        $stmt = $pdo->prepare("SELECT
+                                books.id,
+                                books.title,
+                                books.year_published,
+                                authors.id as author_id,
+                                authors.name as author_name,
+                                authors.bio as author_bio
+                            FROM
+                                books
+                            INNER JOIN
+                                authors
+                            ON
+                                books.author_id = authors.id
+                            WHERE
+                                books.id = :id");
+
+        // Execute the statement
+        $stmt->execute(['id' => $id]);
+
+        // Fetch the data
+        $row = $stmt->fetch();
+
+        // Check for existence of a row
+        if (!$row) {
+            return null;
+        }
+
+        // Use the row of data to build the object(s)
 
         $author = Author::create(
             id: $row['author_id'],
