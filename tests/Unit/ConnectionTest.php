827d5f02c8f425ddabe83dc6803b9360ff1b7af2
Gary Clarke <garyclarke1974@gmail.com>
2023-07-23 19:28:29 +0100

    Write ConnectionTest

---
+++ tests/Unit/ConnectionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+it('returns a valid PDO instance', function() {
+
+    // ARRANGE
+    $connection = new Connection('sqlite::memory:');
+
+    // ACT
+    $pdo = $connection->getPdo();
+
+    // ASSERT
+    expect($pdo)->toBeInstanceOf(PDO::class);
+});
+
