e431df9aabca1d0a84203a0667a2d05ed7820282
Gary Clarke <garyclarke1974@gmail.com>
2023-08-19 18:38:06 +0100

    Add request kernel response steps to index

--- public/index.php
+++ public/index.php
@@ -1,4 +1,21 @@
 <?php
 
-echo 'Hello World';
+// Create a $request using a static named constructor
+use App\Http\Kernel;
+use App\Http\Request;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+
+// Create a Http/Kernel (the heart of the application)
+$kernel = $this->container->get(Kernel::class);
+
+// Call the handle method on the Kernel, passing in the Request..
+//the handle method returns our treasured Response
+// so $response = $kernel->handle($request);
+$response = $kernel->handle($request);
+
+// send back content
+$response->send();
 
