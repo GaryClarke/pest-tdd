b98e85eaa6520faf5b04c9f8cdbbc93b3e95f2e0
Gary Clarke <garyclarke1974@gmail.com>
2023-08-24 20:38:24 +0100

    Add JsonResponse testing to ResponseTest

--- tests/Unit/Http/ResponseTest.php
+++ tests/Unit/Http/ResponseTest.php
@@ -1,13 +1,39 @@
 <?php
 
+use App\Http\Response;
+
 test('a Response object can be created', function() {
 
     // ACT
-    $response = new \App\Http\Response('{"foo":"bar"}', 200);
+    $response = new Response('{"foo":"bar"}', 200);
 
     // ASSERT
     expect($response->getStatusCode())->toBeInt()->toBe(200)
         ->and($response->getBody())
         ->toMatchJson(['foo' => 'bar']);
 });
+
+test('a JsonResponse can be created from an object or an array', function ($body) {
+
+    // ARRANGE
+    // Create a JsonResponse
+    $response = new JsonResponse($body, Response::HTTP_OK);
+
+    // ASSERT (headers, body)
+    expect($response->getHeaders())
+        ->toMatchArray([
+            'Content-Type' => 'application/json'
+        ])
+        ->and($response->getBody())
+        ->toMatchJson(['foo' => 'bar']);
+})->with([
+    'object' => [
+        new class {
+            public string $foo = 'bar';
+        }
+    ],
+    'array' => [
+        ['foo' => 'bar']
+    ]
+]);
 
