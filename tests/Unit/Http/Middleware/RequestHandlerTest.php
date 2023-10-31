3b35a4bc99c5e6d42510f6d134f201ab35ac2234
Gary Clarke <garyclarke1974@gmail.com>
2023-10-31 13:31:28 +0000

    Create RequestHandlerTest

---
+++ tests/Unit/Http/Middleware/RequestHandlerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Response;
+use Unit\Http\Middleware\SuccessMiddleware;
+
+beforeEach(function() {
+    $this->container = include dirname(__DIR__, 4) . '/config/services.php';
+});
+
+test('RequestHandler returns a correct Response', function() {
+
+    // ARRANGE
+    $request = \App\Http\Request::create('GET', '/some/uri');
+
+    $requestHandler = new RequestHandler($this->container);
+
+    $requestHandler->setMiddleware([SuccessMiddleware::class]);
+
+    // ACT
+    $response = $requestHandler->handle($request);
+
+    // ASSERT
+    expect($response)
+        ->toBeInstanceOf(Response::class)
+        ->and($response->getStatusCode())->toBe(200);
+});
+
---
+++ tests/Unit/Http/Middleware/SuccessMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Unit\Http\Middleware;
+
+use App\Http\Middleware\MiddlewareInterface;
+use App\Http\Middleware\RequestHandlerInterface;
+use App\Http\Request;
+use App\Http\Response;
+
+class SuccessMiddleware implements MiddlewareInterface
+{
+    public function process(Request $request, RequestHandlerInterface $handler): Response
+    {
+        return new Response('Success', 200);
+    }
+}
