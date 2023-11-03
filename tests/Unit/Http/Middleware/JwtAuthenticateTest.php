c052cccb9114c30e09a3148a3e2809a4b71394a1
Gary Clarke <garyclarke1974@gmail.com>
2023-11-03 19:26:54 +0000

    CreateJwtAuthenticateTest

---
+++ tests/Unit/Http/Middleware/JwtAuthenticateTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Middleware\RequestHandlerInterface;
+
+test('JWT authentication produces the correct response', function() {
+
+    // ARRANGE
+
+    // Request
+    $request = \App\Http\Request::create(
+        'GET',
+        '/some/path',
+        ['HTTP_AUTHORIZATION' => 'Bearer some.fake.token'],
+    );
+
+    // Middleware
+    $jwtAuth = new JwtAuthenticate('some-secret-key');
+
+    // RequestHandlerInterface
+    $requestHandler = Mockery::mock(RequestHandlerInterface::class);
+
+    // ACT
+    // Middleware process()
+    $response = $jwtAuth->process($request, $requestHandler);
+
+    // ASSERT
+    // Expect 401 response
+    // Invalid token header
+    expect($response)
+        ->toBeInstanceOf(\App\Http\Response::class)
+        ->and($response->getStatusCode())->toBe(401)
+        ->and($response->getHeaders()['WWW-Authenticate'])->toBe('Bearer error="invalid_token"');
+});
+
